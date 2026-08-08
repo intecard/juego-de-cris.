d132629a127f9df849df6aa84244cf062de1e0aa
Luis Ramirez <el-yami11@hotmail.com>
2026-08-08 08:58:57 -0400

    Configuración automática de APK

---
+++ android/app/src/main/java/com/inteca/juego/MainActivity.java
@@ -0,0 +1,5 @@
+package com.inteca.juego;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.inteca.juego',
+  appName: 'Juego de Cris',
+  webDir: 'dist'
+};
+
+export default config;
